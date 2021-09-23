379e70a6a3164cf530b23951f35d0aab4acb690f
RichardGThompson <graham.thompson.613@gmail.com>
2021-09-23 18:44:22 -0400

    First commit

---
+++ scripts/scripts.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', initalizedApp);
+
+function initalizedApp(){
+    displayMeme();
+}
+
+async function displayMeme(){
+    try{
+        const response = await
+        fetch("https://api.imgflip.com/get_memes");
+        const jsonResponse = await response.json();
+        
+        console.log("🚀 ~ file: scripts.js ~ line 12 ~ displayMeme ~ jsonResponse", jsonResponse)
+        
+        const meme = jsonResponse.data.memes[1];
+
+        const image = document.createElement("img");
+        image.src = meme.url;
+        image.alt = meme.name;
+        image.width = 200;
+
+        const memeContainer = document.querySelector("#meme");
+        memeContainer.appendChild(image);
+    }
+    catch(error){
+        console.log("🚀 ~ file: scripts.js ~ line 24 ~ displayMeme ~ error", error)
+    }
+}
